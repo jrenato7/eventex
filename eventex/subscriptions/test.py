daebf0cc6dc9cddc1006eb07311e0adcabee591b
Jose Renato <jose.renato77@gmail.com>
2018-06-29 23:01:43 -0300

    Tela de inscricao

---
+++ eventex/subscriptions/test.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+    def test_get(self):
+        """Get /inscricao/ must return status code 200"""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """Must use subscriptions/subscription_form.html"""
+        self.assertTemplateUsed(self.resp, 'subinscriptions/subscription_form.html')
+
+    def test_html(self):
+        """Html must contain input tags"""
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        """Html must contains csrf"""
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """Context must have subscription form"""
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        """Form must have 4 fields"""
+        """ Saltou o baby steps do SubscriptionForm = None que gera um novo erro is_instance
+        Saltou o subscriptionform só herdado de  forms.forms que gera:
+        AssertionError: Sequences differ: ['name', 'cpf', 'email', 'phone'] != []
+
+        """
+        form = self.resp.context['form']
+        self.assertSequenceEqual(
+            ['name', 'cpf', 'email', 'phone'], list(form.fields))
