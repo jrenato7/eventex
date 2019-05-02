e66d3b14c91ae93778eff5b620a37eef1e7793e1
José Renato Barroso <jose.renato77@gmail.com>
2019-05-02 12:35:31 -0300

    Add admin to subscriptions.

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(verbose_name='Nome', max_length=100)
+    cpf = models.CharField(verbose_name='CPF', max_length=11)
+    email = models.EmailField(verbose_name='e-mail')
+    phone = models.CharField(verbose_name='Telefone', max_length=20)
+    created_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "inscrições"
+        verbose_name = "inscrição"
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.name
