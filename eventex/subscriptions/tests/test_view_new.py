e683532012dce17d87d268bb8e4ff7518f56463e
José Renato Barroso <jose.renato77@gmail.com>
2019-06-05 18:00:57 -0300

    Updated the email and phone fields to become optional.

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -90,7 +90,12 @@ class SubscriptionsNewPostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='José Renato', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
 
+        self.assertContains(response, '<ul class="errorlist nonfield">')
 
 
 
