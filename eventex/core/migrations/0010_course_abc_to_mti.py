2c34b421418eadcdea96241e050bbc81fbf65e15
José Renato Barroso <jose.renato77@gmail.com>
2019-06-30 11:08:58 -0300

    Added backward function.

--- eventex/core/migrations/0010_course_abc_to_mti.py
+++ eventex/core/migrations/0010_course_abc_to_mti.py
@@ -18,6 +18,21 @@ def forward_course_abc_to_mti(apps, schema_editor):
     )
 
 
+def backward_course_abc_to_mti(apps, schema_editor):
+    """
+    ABC: Abstract based class.
+    MTI: Multi table inheritance.
+    For each ABC, create an instance of MTI with all attributes.
+    Save MTI.
+    Associate the speakers from ABC to the MTI.
+    Delete ABC.
+    """
+    copy_src_to_dst(
+        apps.get_model('core', 'Course'),
+        apps.get_model('core', 'CourseOld')
+    )
+
+
 def copy_src_to_dst(Source, Destionation):
     for src in Source.objects.all():
         dst = Destionation(
