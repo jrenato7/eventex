f1c82a37172af224bf21bb16dfbb6a75afec3986
José Renato Barroso <jose.renato77@gmail.com>
2019-06-30 16:57:56 -0300

    Change home view from function based view to class based view.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from eventex.core.models import Speaker, Talk,  Course
 
 
-def home(request):
-    speakers = Speaker.objects.all()
-    return render(request, 'index.html', {'speakers': speakers})
+home = ListView.as_view(template_name='index.html',
+                        model=Speaker)
 
 
 def speaker_detail(request, slug):
