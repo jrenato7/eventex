3c6972d247ecf581806d769c8cce3445b3eeaa42
José Renato Barroso <jose.renato77@gmail.com>
2019-06-30 17:15:04 -0300

    Changed speaker detail view from function based view to class based view.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from eventex.core.models import Speaker, Talk,  Course
-
+from eventex.core.models import Speaker, Talk
 
 home = ListView.as_view(template_name='index.html',
                         model=Speaker)
 
-
-def speaker_detail(request, slug):
-    speaker = get_object_or_404(Speaker, slug=slug)
-    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
-
+speaker_detail = DetailView.as_view(model=Speaker)
 
 talk_list = ListView.as_view(model=Talk)
